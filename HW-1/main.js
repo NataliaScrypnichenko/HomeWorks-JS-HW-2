46b44b0e02370e8fc6fa51bdec0394881ab22bbe
NataliaScrypnichenko <cornanatalia604@gmail.com>
2024-11-04 23:12:16 +0200

    Initial commit

---
+++ HW-1/main.js
@@ -0,0 +1,17 @@
+let s =10;
+console.log(s);
+let z =-99;
+console.log(z);
+let q =123;
+console.log(q);
+const PI=3.14;
+console.log(PI);
+let w =2.7;
+console.log(w);
+let r =16;
+console.log(r);
+
+let t=true;
+console.log(t);
+let y =false;
+console.log(y);

2b36705a08a275fa788c58a5a149ce30667c80ef
NataliaScrypnichenko <cornanatalia604@gmail.com>
2024-11-05 23:06:34 +0200

    виконала

---
+++ HW-2/#jCHFnEbdmFd.js
@@ -0,0 +1,62 @@
+let arr = [
+    user1 ={
+        name: 'Aron',
+        username: 'ARON',
+        password: 'qwru',
+    },
+    user2 ={
+        name: 'Amy',
+        username: 'AMY',
+        password: 'qwr4u',
+    },
+    user3 ={
+        name: 'Adela',
+        username: 'ADELA',
+        password: 'qwr45lu',
+    },
+    user4 ={
+        name: 'Alvin',
+        username: 'ALVIN',
+        password: 'qwr0=nbg',
+    },
+    user5 ={
+        name: 'Basil',
+        username: 'BASIL',
+        password: 'qwr456u',
+    },
+    user6 ={
+        name: 'Barry',
+        username: 'BARRY',
+        password: 'qr456u',
+    },
+    user7 ={
+        name: 'Eddy',
+        username: 'EDDY',
+        password: 'qBVr456u',
+    },
+    user8 ={
+        name: 'Felix',
+        username: 'FELIX',
+        password: 'qEFB456u',
+    },
+    user9 ={
+        name: 'Ian',
+        username: 'IAN',
+        password: 'BHY76R6u',
+    },
+    user10 ={
+        name: 'Hank',
+        username: 'HANK',
+        password: 'LMGR43WS',
+    }
+]
+console.log(user1.password);
+console.log(user2.password);
+console.log(user3.password);
+console.log(user4.password);
+console.log(user5.password);
+console.log(user6.password);
+console.log(user7.password);
+console.log(user8.password);
+console.log(user9.password);
+console.log(user10.password);

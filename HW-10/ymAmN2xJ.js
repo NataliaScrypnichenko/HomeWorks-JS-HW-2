ac2fd56ae8bdaccc9463c3199e36666a28397da2
NataliaScrypnichenko <cornanatalia604@gmail.com>
2024-11-23 21:03:55 +0200

    не пише під об'єктом

---
+++ HW-10/ymAmN2xJ.js
@@ -0,0 +1,16 @@
+//доступитися
+const someForm = document.forms.someForm;
+
+const target  = document.getElementById('target')
+
+someForm.addEventListener( 'submit', (event) =>{
+    event.preventDefault();
+    //зчитати ін-цію за х-кою name із input
+    const nameValue = someForm.name.value;
+    //зчитати ін-цію за х-кою surname із input (є атрибут value)
+    const surnameValue = someForm.surname.value;
+    const ageValue = +someForm.age.value;
+    let obj ={nameValue, surnameValue, ageValue}
+    console.log(obj);
+    target.innerText = obj.nameValue + '' + obj.surnameValue +''+ obj.ageValue
+});
